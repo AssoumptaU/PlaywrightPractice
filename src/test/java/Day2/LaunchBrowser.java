4d4c43ce23f8dcec53cec3468edb01c6460da3b9
Assoumpta <uwasumuta12@gmail.com>
2026-01-30 16:35:42 +0200

    Made some changes on InteractWithInputs

---
+++ src/test/java/Day2/LaunchBrowser.java
@@ -0,0 +1,28 @@
+package Day2;
+
+import com.microsoft.playwright.*;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+
+public class LaunchBrowser {
+     public static void main(String[] args){
+         Playwright playwright = Playwright.create();
+        Browser browser = playwright.firefox().launch(
+                new BrowserType.LaunchOptions().setHeadless(false)
+        );
+        Page page = browser.newPage();
+        page.navigate("https://ecommerce-playground.lambdatest.io/index.php");
+        Locator myAccount = page.locator("//*[@id=\"widget-navbar-217834\"]/ul/li[6]/a/div/span");
+        myAccount.hover();
+        page.locator("//*[@id=\"widget-navbar-217834\"]/ul/li[6]/ul/li[1]/a/div/span").click();
+        assertThat(page).hasTitle("Account Login");
+//         assertThat(page).hasURL("https://ecommerce-playground.lambdatest.io/index.php?route=account/login");
+         page.getByPlaceholder("E-mail Address").type("hel-lo@gmail.com");
+         page.getByPlaceholder("Password").type("12345");
+         page.locator("//input[@value='Login']").click();
+         assertThat(page).hasTitle("My Account");
+         page.close();
+         browser.close();
+         playwright.close();
+     }
+}
---
+++ src/test/java/Day3/InteractWithInputs.java
@@ -0,0 +1,44 @@
+package Day3;
+
+import com.microsoft.playwright.*;
+
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+
+public class InteractWithInputs {
+    public static void main(String[] args) {
+        Playwright playwright = Playwright.create();
+        Browser browser = playwright.chromium().launch(
+                new BrowserType.LaunchOptions().setHeadless(false)
+        );
+        Page page = browser.newPage();
+//        page.navigate("https://www.lambdatest.com/selenium-playground/simple-form-demo/");
+//        page.locator("input#user-message").type("Hey Tester");
+//        page.locator("#showInput").click();
+//        String message = page.locator("#message").textContent();
+//        System.out.println(message);
+//        assertThat(page.locator("#message")).hasText("Hey Tester");
+
+//        playwright.close();
+//        browser.close();
+
+        //Type vs
+        //page.navigate("https://www.testmuai.com/selenium-playground/generate-file-to-download-demo/");
+        //page.locator("#textbox").type("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.");
+        page.navigate("https://letcode.in/edit");
+        String inputValue = page.locator("#getMe").inputValue();
+        System.out.println(inputValue);
+
+        String placeholderValue = page.locator("#fullName").getAttribute("placeholder");
+        System.out.println(placeholderValue);
+
+        Locator fullNamelocator = page.locator("#fullName");
+        assertThat(fullNamelocator).hasAttribute("placeholder", "Enter first & last name");
+
+        page.locator("#clearMe").clear();
+
+    }
+}
+
+//Methods:
+//inputValue: reads and returns the value that's in the input
