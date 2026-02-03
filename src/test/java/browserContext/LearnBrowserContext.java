29bba2004ca65c5932af1a0387f137771e0ef14f
Assoumpta <uwasumuta12@gmail.com>
2026-02-03 15:28:13 +0200

    added a class about browserContext

--- src/test/java/browserContext/LearnBrowserContext.java
+++ src/test/java/browserContext/LearnBrowserContext.java
@@ -20,6 +20,26 @@ public class LearnBrowserContext {
         Locator myAccount = page.getByText("Edit your account information");
         assertThat(myAccount).isVisible();
 
-//        playwright.close();
+//
+
+        //How to Open a new tab with the same browser session
+       Page newTab=  page.context().newPage();
+       newTab.navigate("https://ecommerce-playground.lambdatest.io/index.php?route=account/account");
+        assertThat(myAccount).isVisible();
+        newTab.close();
+        context.close();
+
+        //Opening a new BrowserContext
+        BrowserContext context2 = browser.newContext();
+       Page userPage = context2.newPage();
+       userPage.navigate("https://ecommerce-playground.lambdatest.io/index.php?route=account/account");
+
+       //Opening Multiple browsers
+        BrowserType firefox = playwright.firefox();
+        firefox.launch(
+                new BrowserType.LaunchOptions().setHeadless(false)
+        ).newPage();
+
+        playwright.close();
     }
 }
